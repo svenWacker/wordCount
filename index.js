1b2cd1ff29f0e07992d93d1558b637efe5201a88
svenWacker <s.u.wacker@outlook.de>
2021-02-16 14:40:51 +0100

    solution in firstbranch

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// first branch
 const sentence =
   "This is the assignment to count the words of a sentence in three ways";
 
-function wordCountUsingSplit(str) {
-  let words = str.split(" ");
-  if (str == 0 || words == ``) {
-    console.log(`No words`);
-  } else {
-    console.log(`There are ${words.length} words`);
-  }
-}
-wordCountUsingSplit(sentence);
-wordCountUsingSplit("    ");
+// first branch
+// function wordCountUsingSplit(str) {
+//   let words = str.split(" ");
+//   if (str == 0 || words == ``) {
+//     console.log(`No words`);
+//   } else {
+//     console.log(`There are ${words.length} words`);
+//   }
+// }
+// wordCountUsingSplit(sentence);
+// wordCountUsingSplit("    ");
+
+// second branch
+console.log("Word count is = " + sentence.split(" ").length);
