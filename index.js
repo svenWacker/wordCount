c63ed430657e7d55b8dd01df29219dec8bff951a
svenWacker <s.u.wacker@outlook.de>
2021-02-16 15:27:08 +0100

    assignment with branches

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // first line
 // assignment with branches
+
+const sentence =
+  "This is the assignment to count the words of a sentence in three ways";
+function countWords(str) {
+  return str.trim().split(/\s+/).length;
+}
+console.log(countWords(sentence));
